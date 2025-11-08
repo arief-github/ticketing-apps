25cb9cb34367c4b10e37d7b9ed5ba5f5f41fa1e7
arief-github <ariefkurniawan0411@gmail.com>
2025-11-09 02:01:28 +0700

    feat: add user and session models, update navigation and paths for authentication

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,29 +1,49 @@
-import { Tickets } from "lucide-react"
-import Link from "next/link"
+import { Tickets } from "lucide-react";
+import Link from "next/link";
 
-import { homePath, ticketsPath } from "@/paths"
+import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
 
-import { ThemeSwitcher } from "../theme/theme-switcher"
-import { buttonVariants } from "../ui/button"
+import { ThemeSwitcher } from "../theme/theme-switcher";
+import { buttonVariants } from "../ui/button";
 
 const Navigation = () => {
-    return (
-        <nav className="
+  return (
+    <nav
+      className="
           supports-backdrop-blur:bg-background/60
           fixed left-0 right-0 top-0 z-20
           border-b bg-background/95 backdrop-blur
           w-full flex py-2.5 px-5 justify-between
-        ">
-            <Link href={homePath()} className={buttonVariants({ variant: 'ghost' })}>
-                <Tickets />
-                <h1 className="ml-2 text-lg font-semibold">TicketsHome</h1>
-            </Link>
-            <div className="flex items-center gap-x-1.5">
-                <ThemeSwitcher />
-                <Link href={ticketsPath()} className={buttonVariants({ variant: 'default' })}>Tickets</Link>
-            </div>
-        </nav>
-    )
-}
+        "
+    >
+      <Link href={homePath()} className={buttonVariants({ variant: "ghost" })}>
+        <Tickets />
+        <h1 className="ml-2 text-lg font-semibold">TicketsHome</h1>
+      </Link>
+      <div className="flex items-center gap-x-1.5">
+        <ThemeSwitcher />
+        <Link
+          href={ticketsPath()}
+          className={buttonVariants({ variant: "default" })}
+        >
+          Tickets
+        </Link>
+        <Link
+          href={signUpPath()}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          Sign Up
+        </Link>
 
-export default Navigation
+        <Link
+          href={signInPath()}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
