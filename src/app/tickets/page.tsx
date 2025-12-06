f8e0ea27a89d59774a96dce135c365d46ca92b85
arief-github <ariefkurniawan0411@gmail.com>
2025-12-06 16:12:01 +0700

    feat: Enable user-specific ticket filtering, displaying all tickets on the homepage and authenticated user's tickets on their dedicated page.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,28 +1,34 @@
-import {Suspense} from "react";
+import { Suspense } from "react";
 
-import {CardFormTicket} from "@/components/composition/CardFormTicket";
-import Heading from '@/components/shared/Heading';
+import { CardFormTicket } from "@/components/composition/CardFormTicket";
+import Heading from "@/components/shared/Heading";
 import Spinner from "@/components/shared/Spinner";
-import {TicketList} from '@/features/ticket/components/ticket-list';
-import {TicketUpsertForm} from "@/features/ticket/components/ticket-upsert-form";
+import { getAuth } from "@/features/auth/actions/get-auth";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
 const TicketsPage = async () => {
-    return (
-        <div className="flex-1 flex flex-col gap-y-8">
-            <Heading title='Tickets Page' description=' All your tickets at one place'/>
+  const { user } = await getAuth();
 
-            <CardFormTicket
-                title="Create Ticket"
-                description="A new ticket will be created"
-                className="w-full max-w-[420px] self-center"
-                content={<TicketUpsertForm/>}
-            />
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading
+        title="Tickets Page"
+        description=" All your tickets at one place"
+      />
 
-            <Suspense fallback={<Spinner/>}>
-                <TicketList/>
-            </Suspense>
-        </div>
-    )
-}
+      <CardFormTicket
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketUpsertForm />}
+      />
+
+      <Suspense fallback={<Spinner />}>
+        <TicketList userId={user?.id} />
+      </Suspense>
+    </div>
+  );
+};
 
 export default TicketsPage;
