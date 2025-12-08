2e1cc7b5b4639073ae398eeda327ca3f6e437684
arief-github <ariefkurniawan0411@gmail.com>
2025-12-09 05:05:37 +0700

    feat: Implement account navigation tabs and integrate them into profile and password pages.

--- src/app/account/password/page.tsx
+++ src/app/account/password/page.tsx
@@ -1,9 +1,14 @@
 import Heading from "@/components/shared/Heading";
+import AccountTabs from "@/features/account/components/account-tabs";
 
 const PasswordPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <Heading title="Password" description="All your password information" />
+      <Heading
+        title="Password"
+        description="All your password information"
+        tabs={<AccountTabs />}
+      />
     </div>
   );
 };
--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -1,9 +1,14 @@
 import Heading from "@/components/shared/Heading";
+import AccountTabs from "@/features/account/components/account-tabs";
 
 const ProfilePage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <Heading title="Profile" description="All your profile information" />
+      <Heading
+        title="Profile"
+        description="All your profile information"
+        tabs={<AccountTabs />}
+      />
     </div>
   );
 };
