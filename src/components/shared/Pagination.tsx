a0bd1f2a0f3a02eb2b3f720809f40c8590494df2
arief-github <ariefkurniawan0411@gmail.com>
2025-12-14 21:09:07 +0700

    feat: add page size selection and disabled cursor styling to the Pagination component

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -1,4 +1,11 @@
 import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 type PageAndSize = Record<"page" | "size", number>;
 
@@ -30,12 +37,17 @@ const Pagination = ({
     onPagination({ ...pagination, page: pagination.page + 1 });
   };
 
+  const handleChangeSize = (value: string) => {
+    onPagination({ page: 0, size: parseInt(value) });
+  };
+
   const previousButton = (
     <Button
       variant="outline"
       size="sm"
       disabled={pagination.page < 1}
       onClick={handlePreviousPage}
+      className="disabled:cursor-not-allowed"
     >
       Prev
     </Button>
@@ -47,6 +59,7 @@ const Pagination = ({
       size="sm"
       disabled={!hasNextPage}
       onClick={handleNextPage}
+      className="disabled:cursor-not-allowed"
     >
       Next
     </Button>
@@ -56,6 +69,26 @@ const Pagination = ({
     <div className="flex items-center justify-between">
       <p className="text-sm text-muted-foreground">{label}</p>
       <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2">
+          <p className="text-sm text-muted-foreground whitespace-nowrap">
+            Rows per page
+          </p>
+          <Select
+            value={pagination.size.toString()}
+            onValueChange={handleChangeSize}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={pagination.size.toString()} />
+            </SelectTrigger>
+            <SelectContent>
+              {[2, 5, 10, 25, 50, 100].map((size) => (
+                <SelectItem key={size} value={size.toString()}>
+                  {size}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
         {previousButton}
         {nextButton}
       </div>
