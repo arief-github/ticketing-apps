e4699f3eea5b9307d281ae9703eb9b28bf32a695
arief-github <ariefkurniawan0411@gmail.com>
2026-02-09 03:23:16 +0700

    feat: associate tickets with an active organization and enforce its selection during creation and authentication.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,114 +1,115 @@
 import { hash } from "@node-rs/argon2";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 type InitialTicket = {
-    title: string;
-    content: string;
-    status: 'DONE' | 'OPEN' | 'IN_PROGRESS'
-    deadline: string
-    bounty: number
-}
+  title: string;
+  content: string;
+  status: "DONE" | "OPEN" | "IN_PROGRESS";
+  deadline: string;
+  bounty: number;
+};
 
 const users = [
-    {
-        username: 'johndoe',
-        email: 'john@example.com',
-    },
-    {
-        username: 'arief-kurniawan',
-        email: 'ariefgisdev@gmail.com',
-    },
-    {
-        username: 'admin',
-        email: 'admin@example.com',
-    }
-]
+  {
+    username: "johndoe",
+    email: "john@example.com",
+  },
+  {
+    username: "arief-kurniawan",
+    email: "ariefgisdev@gmail.com",
+  },
+  {
+    username: "admin",
+    email: "admin@example.com",
+  },
+];
 
 export const initialData: InitialTicket[] = [
-    {
-        title: 'Tiket Rahmatraja',
-        content: 'Siap menggetarkan bandung',
-        status: "DONE",
-        deadline: new Date().toISOString().split("T")[0],
-        bounty: 399
-    },
-    {
-        title: 'Tiket Atma Asta',
-        content: 'Bandung in harmony',
-        status: "OPEN",
-        deadline: new Date().toISOString().split("T")[0],
-        bounty: 499
-    },
-    {
-        title: 'Reckastra',
-        content: 'Bandung in Code',
-        status: "IN_PROGRESS",
-        deadline: new Date().toISOString().split("T")[0],
-        bounty: 299
-    }
-]
+  {
+    title: "Tiket Rahmatraja",
+    content: "Siap menggetarkan bandung",
+    status: "DONE",
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 399,
+  },
+  {
+    title: "Tiket Atma Asta",
+    content: "Bandung in harmony",
+    status: "OPEN",
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 499,
+  },
+  {
+    title: "Reckastra",
+    content: "Bandung in Code",
+    status: "IN_PROGRESS",
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 299,
+  },
+];
 
 const comments = [
-    { content: "First Comment on DB" },
-    { content: "Second Comment on DB" },
-    { content: "Third Comment on DB" },
-]
+  { content: "First Comment on DB" },
+  { content: "Second Comment on DB" },
+  { content: "Third Comment on DB" },
+];
 
 // make async function to store initialData into supabase server
 const seed = async () => {
-    await prisma.comment.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.ticket.deleteMany();
-    await prisma.organization.deleteMany();
-    await prisma.membership.deleteMany();
+  await prisma.comment.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.ticket.deleteMany();
+  await prisma.organization.deleteMany();
+  await prisma.membership.deleteMany();
 
-    const passwordHash = await hash("superscret")
+  const passwordHash = await hash("superscret");
 
-    const dbUser = await prisma.user.createManyAndReturn({
-        data: users.map(user => ({
-            ...user,
-            passwordHash: passwordHash
-        }))
-    })
+  const dbUser = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({
+      ...user,
+      passwordHash: passwordHash,
+    })),
+  });
 
-    const dbOrganization = await prisma.organization.create({
+  const dbOrganization = await prisma.organization.create({
     data: {
       name: "Organization Kelas Kakap",
-        },
-    });
+    },
+  });
 
-    await prisma.membership.createMany({    
-        data: [
-            {
-             userId: dbUser[1].id,
-             organizationId: dbOrganization.id,
-             isActive: true,
-            },
-            {
-             userId: dbUser[0].id,
-             organizationId: dbOrganization.id,
-             isActive: true,
-            }
-        ]
-    });
-    
-    const dbTickets = await prisma.ticket.createManyAndReturn({
-        data: initialData.map(ticket => ({
-            ...ticket,
-            userId: dbUser[0].id
-        }))
-    })
+  await prisma.membership.createMany({
+    data: [
+      {
+        userId: dbUser[1].id,
+        organizationId: dbOrganization.id,
+        isActive: true,
+      },
+      {
+        userId: dbUser[0].id,
+        organizationId: dbOrganization.id,
+        isActive: true,
+      },
+    ],
+  });
 
-    await prisma.comment.createMany({
-        data: comments.map((comment) => ({
-            ...comment,
-            userId: dbUser[0].id,
-            ticketId: dbTickets[0].id
-        }))
-    })
-}
+  const dbTickets = await prisma.ticket.createManyAndReturn({
+    data: initialData.map((ticket) => ({
+      ...ticket,
+      userId: dbUser[0].id,
+      organizationId: dbOrganization.id,
+    })),
+  });
+
+  await prisma.comment.createMany({
+    data: comments.map((comment) => ({
+      ...comment,
+      userId: dbUser[0].id,
+      ticketId: dbTickets[0].id,
+    })),
+  });
+};
 
 // executed the seed function
-seed()
+seed();
--- src/features/auth/queries/get-auth-or-redirect.ts
+++ src/features/auth/queries/get-auth-or-redirect.ts
@@ -1,41 +1,53 @@
 import { redirect } from "next/navigation";
 
 import { getOrganizationByUser } from "@/features/organization/queries/get-organization-by-user";
-import { emailVerificationPath, onboardingPath, selectActiveOrganizationPath,signInPath} from "@/paths";
+import {
+  emailVerificationPath,
+  onboardingPath,
+  selectActiveOrganizationPath,
+  signInPath,
+} from "@/paths";
 
 import { getAuth } from "../actions/get-auth";
 
 type GetAuthRedirectOptions = {
-    checkEmailVerified?: boolean;
-    checkOrganization?: boolean;
-    checkActiveOrganization?: boolean;
-}
+  checkEmailVerified?: boolean;
+  checkOrganization?: boolean;
+  checkActiveOrganization?: boolean;
+};
 
-export const getAuthOrRedirect = async ({ checkEmailVerified = true, checkOrganization = true, checkActiveOrganization = true }: GetAuthRedirectOptions = {}) => {
-    const auth = await getAuth();
-    if (!auth.user) {
-        redirect(signInPath());
+export const getAuthOrRedirect = async ({
+  checkEmailVerified = true,
+  checkOrganization = true,
+  checkActiveOrganization = true,
+}: GetAuthRedirectOptions = {}) => {
+  const auth = await getAuth();
+  if (!auth.user) {
+    redirect(signInPath());
+  }
+
+  if (checkEmailVerified && !auth.user.emailVerified) {
+    redirect(emailVerificationPath());
+  }
+
+  let activeOrganization;
+
+  if (checkOrganization || checkActiveOrganization) {
+    const organizations = await getOrganizationByUser();
+    if (checkOrganization && !organizations.length) {
+      redirect(onboardingPath());
     }
 
-    if (checkEmailVerified && !auth.user.emailVerified) {
-        redirect(emailVerificationPath());
+    activeOrganization = organizations.find((org) => {
+      return org.membershipByUser.isActive;
+    });
+
+    const hasActive = !!activeOrganization;
+
+    if (!hasActive && checkActiveOrganization) {
+      redirect(selectActiveOrganizationPath());
     }
+  }
 
-    
-    if(checkOrganization || checkActiveOrganization) {
-        const organizations = await getOrganizationByUser()
-        if(checkOrganization && !organizations.length) {
-            redirect(onboardingPath())
-        }
-
-        const hasActive = organizations.some((org) => {
-            return org.membershipByUser.isActive
-        })
-
-        if(!hasActive && checkActiveOrganization) {
-            redirect(selectActiveOrganizationPath())
-        }
-    }
-
-    return auth;
+  return { ...auth, activeOrganization };
 };
